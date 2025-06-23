88e3df911e44d9c9d4cfe5fce83b28bfa67e4c9d
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-06-23 20:31:34 +0200

    imports all icons on init and displays on match

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,5 +1,11 @@
 import { getWeatherData } from "./weather";
 
+let icons = {};
+
+function importAll(r) {
+  r.keys().forEach((key) => (icons[key] = r(key)));
+}
+
 const contentDiv = document.getElementById("content");
 const searchBtn = document.getElementById("searchbtn");
 const locationInput = document.getElementById("location");
@@ -16,16 +22,21 @@ const sendRequest = async function () {
 const weatherDom = (weatherData) => {
   const dayWeather = weatherData.days[0];
   const day1Container = document.getElementById("dayOne");
-    //chooses div for date
+  //chooses div for date
   day1Container.firstElementChild.firstElementChild.textContent =
     dayWeather.datetime;
-  //chooses div for weather icon. function for implementing right still to add
-  day1Container.firstElementChild.lastElementChild.textContent =
-    dayWeather.icon;
-    day1Container.lastElementChild.firstElementChild.textContent = dayWeather.tempmax + "°C";
-    day1Container.lastElementChild.lastElementChild.textContent = dayWeather.conditions;
+  //chooses div for weather icon. function for implementing right icon still to add
+  day1Container.firstElementChild.lastElementChild.src =
+    icons[`./${dayWeather.icon}.png`];
+
+  day1Container.lastElementChild.firstElementChild.textContent =
+    dayWeather.tempmax + "°C";
+  day1Container.lastElementChild.lastElementChild.textContent =
+    dayWeather.conditions;
 };
 
 export const init = () => {
+  importAll(require.context("../icons", false, /\.png$/));
   searchBtn.addEventListener("click", sendRequest);
 };
+
